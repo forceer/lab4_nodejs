971a06ef1eaabc6d0a0960d232a1ab7d41f07e1a
forceer <anton.klimchuk.2004@gmail.com>
2025-04-25 02:31:28 +0300

    logger ready

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import requestLogger from './middleware/requestLogger';
+import errorHandler from './middleware/errorHandler';
 import userRouter from './routes/user.routes';
- import postRouter from './routes/post.routes';
- import commentRouter from './routes/comment.routes';
+import postRouter from './routes/post.routes';
+import commentRouter from './routes/comment.routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(requestLogger);
 
 app.use('/', (req, res, next) => {
   if (req.originalUrl === '/') {
@@ -18,5 +21,7 @@ app.use('/', (req, res, next) => {
 app.use('/users', userRouter);
 app.use('/posts', postRouter);
 app.use('/comments', commentRouter);
+
+app.use(errorHandler);
 
 export default app;
